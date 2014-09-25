175cb48c4048238710f9ca6efe127cac70f7153e
Aaron Lun <alun@wehi.edu.au>
2014-09-25 11:35:15 +1000

    Changed countPairs function to prunePairs, switched to param support, optimized some code.

--- package/R/countPairs.R
+++ package/R/countPairs.R
@@ -1,27 +1,8 @@
-.sortedAggregate <- function(x, mode=c("count", "sum"))
-# This is an efficient version of 'aggregate(count/length~anchor.id+target.id, data=x, FUN=sum/length)'
-# which takes advantage of the fact that x is sorted by anchor/target.id values.
-{
-	if (!nrow(x)) { 
-		return(data.frame(anchor.id=integer(0), target.id=integer(0), count=integer(0)))
-	}
-	mode <- match.arg(mode)
-	delta.a <- diff(x$anchor.id)
-	delta.t <- diff(x$target.id)
-	if (any(delta.a<0L) | any(delta.a==0L & delta.t<0L)) { stop("anchor/target indices should be sorted") }
-	last.one <- which(c(delta.a!=0L | delta.t!=0L, TRUE))
-		
-	if (mode=="count")  { collected <- last.one }
-	else if (mode=="sum") { collected <- cumsum(x$count)[last.one] }
-	counts <- collected-c(0L, collected[-length(collected)])
-	data.frame(anchor.id=x$anchor.id[last.one], target.id=x$target.id[last.one], count=counts)
-}
-
-countPairs <- function(file.in, file.out=file.in, max.length=NA, min.ingap=NA, min.outgap=NA)
+prunePairs <- function(file.in, param, file.out=file.in, max.frag=NA, min.inward=NA, min.outward=NA)
 # This function collates all valid HiC pairs into a set of counts for each interaction. Basically, it 
 # takes the directory output by 'prepareHiCPairs' and converts it into counts for each interaction. 
 # If the fragment size is too big then we filter it out. We also filter out pairs which are too
-# close together (min.*gap) with the difference between the two specifying inward/outward facing pairs.
+# close together (min.*ward) with the difference between the two specifying inward/outward facing pairs.
 #
 # writtn by Aaron Lun
 {
@@ -38,35 +19,40 @@ countPairs <- function(file.in, file.out=file.in, max.length=NA, min.ingap=NA, m
 		current <- allstuff[[ax]]
 		loaded <- FALSE
 		for (tx in names(current)) { 
-			x <- .getPairs(file.in, ax, tx)
+			collected <- .getPairs(file.in, ax, tx)
+			stats <- .getStats(collected, ax==tx, param$fragments)
 
-			if (!is.na(max.length)) { 
-				keep.len <- x$length <= max.length
+			if (!is.na(max.frag)) { 
+				keep.len <- stats$length <= max.frag
 				by.len <- by.len + sum(!keep.len)
 			} else { keep.len <- TRUE }
-			if (!is.na(min.ingap)) { 
-				keep.in <- x$orientation!=1L | is.na(x$gap) | x$gap >= min.ingap
-				by.in <- by.in + sum(!keep.in)
-			} else { keep.in <- TRUE }
-			if (!is.na(min.outgap)) { 
-				keep.out <- x$orientation!=2L | is.na(x$gap) | x$gap >= min.outgap
-				by.out <- by.out + sum(!keep.out)
-			} else { keep.out <- TRUE }
 
-			total <- total + nrow(x) 
-			x <- x[keep.len & keep.in & keep.out,]
-			if (nrow(x)) { 
+			keep.in <- keep.out <- TRUE
+			if (ax==tx) { 
+				if (!is.na(min.inward)) { 
+					keep.in <- stats$orientation!=1L | stats$insert >= min.inward
+					by.in <- by.in + sum(!keep.in)
+				}
+				if (!is.na(min.outward)) { 
+					keep.out <- stats$orientation!=2L | stats$insert >= min.outward
+					by.out <- by.out + sum(!keep.out)
+				}
+			} 
+
+			total <- total + nrow(collected) 
+			collected <- collected[keep.len & keep.in & keep.out,]
+			if (nrow(collected)) { 
 				if (!loaded) { # Only adding a group if the data.frame is non-empty.
 					.addGroup(tmpf, ax)
 					loaded <- TRUE
 				}
-				.writePairs(.sortedAggregate(x, mode="count"), tmpf, ax, tx)
-				retained <- retained + nrow(x)
+				.writePairs(collected, tmpf, ax, tx)
+				retained <- retained + nrow(collected)
 			}
 		}
 	}
 	
 	# Shuffling things around.
 	if (!file.rename(tmpf, file.out)) { stop("cannot move file to the specified destination") }
-	return(c(total=total, length=by.len, ingap=by.in, outgap=by.out, retained=retained))
+	return(c(total=total, length=by.len, inward=by.in, outward=by.out, retained=retained))
 }
