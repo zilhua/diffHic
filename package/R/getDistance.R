0c4e198c31b257881628ff2934c8fb369a294bda
Aaron Lun <alun@wehi.edu.au>
2015-03-06 11:45:33 +1100

    Clarified comments for getArea, updated UG for filtering functions.

--- package/R/getDistance.R
+++ package/R/getDistance.R
@@ -27,15 +27,13 @@ getDistance <- function(data, type=c("mid", "gap", "span"))
 getArea <- function(data, bp=TRUE)
 # Computes the area of the interaction space, either in base pair-squared terms
 # or in terms of pairs of restriction fragments. This allows adjustment of
-# abundances for comparison between differently-sized areas.  Special behaviour
-# is necessary on the diagonal, as only half the fragments are actually used.
-# 'fragments' should be specified if there is any risk of partial overlaps.
-# Coercion to double is necessary in case the number of fragments overflows the
-# integer type.
+# abundances for comparison between differently-sized areas. Special behaviour
+# is necessary on the diagonal, as reflection halves the space. Coercion to 
+# double is necessary to prevent overflows of the integer type.
 # 
 # written by Aaron Lun
 # created 30 July 2014
-# last modified 3 March 2015
+# last modified 5 March 2015
 {
 	ax <- data@anchors
 	tx <- data@targets
@@ -44,7 +42,8 @@ getArea <- function(data, bp=TRUE)
 		cur.width <- as.double(width(data@regions))
 		returned <- cur.width[ax] * cur.width[tx]
 
-		# Accounting for special behaviour around the diagonal.		
+		# Accounting for special behaviour around the diagonal.	It you don't halve,
+		# you'll have to double every other (unreflected) area.
 		overlap <- getDistance(data, type="gap")
 		is.olap <- !is.na(overlap) & overlap < -0.5
 		lap.dist <- abs(overlap)[is.olap]
@@ -57,9 +56,6 @@ getArea <- function(data, bp=TRUE)
 		returned[is.same] <- curnfrag[is.same]*(curnfrag[is.same]+1)/2
 
 		# Detour to protect against overlapping regions.
-		fragments <- exptData(data)$param$fragments
-		fdata <- .delimitFragments(fragments)
-
 		left.edge <- pmax(start(data@regions)[ax], start(data@regions)[tx])
 		right.edge <- pmin(end(data@regions)[ax], end(data@regions)[tx])
 		is.partial <- !is.same & right.edge >= left.edge & 
@@ -69,6 +65,8 @@ getArea <- function(data, bp=TRUE)
 			right.edge <- right.edge[is.partial]
 			left.edge <- left.edge[is.partial]
 			by.chr <- split(1:sum(is.partial), as.character(seqnames(data@regions)[ax][is.partial]))
+			fragments <- exptData(data)$param$fragments
+			fdata <- .delimitFragments(fragments)
 
 			for (x in 1:length(fdata$chr)) {
 				current.chr <- fdata$chr[x]
