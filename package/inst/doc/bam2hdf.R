900790a9cd4ada9a676f495e90f59f39e1e87672
Aaron Lun <alun@wehi.edu.au>
2015-03-21 10:04:52 +1100

    Fixed bug in BAM to HDF script merging.

--- package/inst/doc/bam2hdf.R
+++ package/inst/doc/bam2hdf.R
@@ -26,9 +26,9 @@ for (i in 1:2) {
 
 	# Merging technical replicates.
 	if (i==1L) { 
-		mergePairs(c("SRR493820.h5", "SRR493821.h5", "SRR493822.h5", "SRR493823.h5"), "counts/merged_erg.h5")
-		mergePairs(c("SRR493824.h5", "SRR493825.h5", "SRR493826.h5", "SRR493827.h5"), "counts/merged_gfp.h5")
-	} else if (i==1L) {
+		mergePairs(c("SRR493820.h5", "SRR493821.h5", "SRR493822.h5", "SRR493823.h5"), "merged_erg.h5")
+		mergePairs(c("SRR493824.h5", "SRR493825.h5", "SRR493826.h5", "SRR493827.h5"), "merged_gfp.h5")
+	} else if (i==2L) {
 		mergePairs(c("SRR941267.h5", "SRR941268.h5", "SRR941269.h5", "SRR941270.h5"), "merged_flox_1.h5")
 		mergePairs(c("SRR941271.h5", "SRR941272.h5", "SRR941273.h5", "SRR941274.h5"), "merged_flox_2.h5")
 		mergePairs(c("SRR941275.h5", "SRR941276.h5", "SRR941277.h5", "SRR941278.h5"), "merged_ko_1.h5")
