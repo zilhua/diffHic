1c17b475b686011f3aea1e9d6b097f3c5aacf123
Aaron Lun <alun@wehi.edu.au>
2015-03-22 11:52:04 +1100

    Added protection against consecutive sites for sites with full 5' overhang.

--- package/R/cutGenome.R
+++ package/R/cutGenome.R
@@ -6,7 +6,7 @@ cutGenome <- function(bs, pattern, overhang=4L)
 #
 # written by Aaron Lun
 # a long time ago. 
-# last modified 2 November 2014
+# last modified 22 March 2014
 {
 	if (nchar(pattern)%%2L!=0) { stop("recognition site must be even in size") }
 	ps <- DNAString(pattern)
@@ -24,10 +24,33 @@ cutGenome <- function(bs, pattern, overhang=4L)
 		ref.names <- names(bs)
 		gen <- NA
 	}	
+
 	for (chr in ref.names) {
        	x <- matchPattern(pattern, bs[[chr]])
-       	starts <- c(1L, start(x)+remainder)
-       	ends <- c(start(x)+remainder-1L+overhang, length(bs[[chr]]))
+		match.start <- start(x)
+		if (is.unsorted(match.start)) { match.start <- sort(match.start) }
+       	starts <- c(1L, match.start+remainder)
+		chrlen <- length(bs[[chr]])
+       	ends <- c(match.start+remainder-1L+overhang, chrlen)
+
+		# Eliminating fragments formed by consecutive matches at the start or end.
+		# These will be ssDNA, so there's nothing to prime the fill-in. 
+		if (remainder==0L) {
+			keep <- NULL
+			if (ends[1]==nchar(pattern)) { keep <- -1L }
+			N <- length(ends)
+			if (starts[N]==chrlen-nchar(pattern)+1L) { keep <- c(keep, -N) }
+
+			if (length(keep)) { 
+				starts <- starts[keep]
+				ends <- ends[keep]
+			} 
+			if (length(starts)==0L) { 
+				starts <- 1L
+				ends <- chrlen
+			}
+		}
+
 		original[[chr]] <- GRanges(chr, IRanges(starts, ends))
 	}
 		
@@ -37,4 +60,11 @@ cutGenome <- function(bs, pattern, overhang=4L)
     suppressWarnings(seqinfo(original) <- Seqinfo(ref.names, seqlengths=seqlengths(bs), genome=gen))
 	return(original)
 }
+
+# We completely ignore tricky interpretations of consecutive sites.
+# For starters, the 'remainder' is so low that the strands are unlikely to stay stuck together until the fill-in step.
+# It's also unclear whether cleavage is even possible when the recognition site is at the very end of the DNA fragment.
+# This is far too much technical detail to be relevant in non-pathological cases.
+# Consideration is only given to the start and end, above, to pass pairParam ordering checks and avoid redundancies.
+
 
