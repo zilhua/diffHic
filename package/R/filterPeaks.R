7c301f1f903e6f01679cc558a9e15d2834a7428f
Aaron Lun <alun@wehi.edu.au>
2015-03-23 17:41:32 +1100

    Modified interface, removed distance correction for filterPeaks.

--- package/R/filterPeaks.R
+++ package/R/filterPeaks.R
@@ -1,12 +1,11 @@
-filterPeaks <- function(data, flank=5, trend=c("global", "none", "chr"), prior.count=2, span=0.3)
+filterPeaks <- function(data, flank=5, prior.count=2, abundances=NULL)
 # This function identifies the highest-abundance neighbour in the interaction space
 # for each bin pair in `data`. The aim is to compare the abundance of each element
-# with the abundance of its neighbour. Some adjustment is necessary to account for
-# the effect of distance on abundance when comparing between distances.
+# with the abundance of its neighbour. 
 #
 # written by Aaron Lun
 # created 23 April 2014
-# last modified 20 March 2015
+# last modified 23 March 2015
 {
 	flank <- as.integer(flank)
 	if (flank <= 0L) { stop("flank width must be a positive integer") }
@@ -15,29 +14,9 @@ filterPeaks <- function(data, flank=5, trend=c("global", "none", "chr"), prior.c
 	first.id <- rdata$start
 	names(last.id) <- names(first.id) <- rdata$chr
 
-	# Instantiating variables.
-	trend <- match.arg(trend)
-	if (trend=="none") { 
-		all.ab <- aveLogCPM(asDGEList(data), prior.count=0)
-    	fitted <- numeric(nrow(data))
-	} else {
-		width <- exptData(data)$width
-		if (is.null(width)) { stop("data must contain bin pair counts") }
-   		log.dist <- log10(getDistance(data) + width)
-		is.intra <- !is.na(log.dist)
-		fitted <- all.ab <- numeric(nrow(data))
-		if (!all(is.intra)) { 
-			# Don't worry about the lack of prior, we never compare prior-adjusted values with these guys.
-			all.ab[!is.intra] <- aveLogCPM(asDGEList(data[!is.intra,]), prior.count=0)
-		}
-		if (trend=="global") { 
-			# Need a prior here, to avoid big adjusted values at high distances.
-			adj.ab <- aveLogCPM(asDGEList(data[is.intra,]), prior.count=prior.count)
-			all.ab[is.intra] <- adj.ab
-			fitted[is.intra] <- loessFit(y=adj.ab, x=log.dist[is.intra], span=span)$fitted
-		}
-	}
-	
+	all.ab <- abundances
+	if (is.null(all.ab)) { all.ab <- aveLogCPM(asDGEList(data), prior.count=0) }
+
 	# Rescaling to count-level data with at least 6 dp, for stable calculations with integers.
 	# This should be enough precision while avoiding overrun of the integer type.
 	scaling <- log2(mean(data$totals)/1e6 * ncol(data))
@@ -68,17 +47,6 @@ filterPeaks <- function(data, flank=5, trend=c("global", "none", "chr"), prior.c
 			t.len <- last.id[[target]] - first.id[[target]] + 1L
 
 			rel.ab <- all.ab[current.pair]
-			if (target==anchor) {
-				# Adjusting abundances for distance. 
-				if (trend=="chr") { 
-					if (length(current.pair)>1L) { 
-						adj <- loessFit(y=rel.ab, x=log.dist[current.pair], span=span)$fitted
-						rel.ab <- rel.ab - adj
-					} else {
-						rel.ab <- 0 # No difference relative to itself.
-					}	
-				} else if (trend=="global") { rel.ab <- rel.ab - fitted[current.pair] }
-			} 
 			converted <- back2count(rel.ab) 
 
 			# Using the quadrant with the maximum average.
