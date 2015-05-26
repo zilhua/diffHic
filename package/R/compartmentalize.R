9c2b8b28d3f520a81f57f07e57d6665976c34d7e
Aaron Lun <alun@wehi.edu.au>
2015-05-26 19:01:12 +1000

    Added protection for small chrs, debiasing of contact matrix.

--- package/R/compartmentalize.R
+++ package/R/compartmentalize.R
@@ -1,4 +1,4 @@
-compartmentalize <- function(data, centers=3, inter=FALSE, ...)
+compartmentalize <- function(data, centers=2, inter=FALSE, ...)
 # Computes compartments for every chromosome, using the intra-chromosomal
 # contact maps that have been corrected for distance effects.
 #
@@ -22,10 +22,10 @@ compartmentalize <- function(data, centers=3, inter=FALSE, ...)
 			mat <- as.matrix(data, first=chr, fill=resids)
 			if (any(dim(mat)==0L)) { next }
 			mat <- .fillZeros(mat, data, dist2trend)
+			mat <- .debiasBins(mat)
 					
 			# Actually partitioning by location.
-			out <- kmeans(mat, centers=centers, ...)
-			comp <- out$cluster
+			comp <- .safeCluster(mat, centers=centers, ...)
 			names(comp) <- rownames(mat)
 			stored[[chr]] <- list(compartment=comp, matrix=mat)
 		}
@@ -33,8 +33,9 @@ compartmentalize <- function(data, centers=3, inter=FALSE, ...)
 		# Using the entirety of the interaction space.				
 		mat <- as.matrix(data, fill=resids)
 		mat <- .fillZeros(mat, data, dist2trend)
-		out <- kmeans(mat, centers=centers, ...)
-		stored <- list(compartment=out$cluster, matrix=mat)
+		mat <- .debiasBins(mat)
+		comp <- .safeCluster(mat, centers=centers, ...)
+		stored <- list(compartment=comp, matrix=mat)
 	}
 
 	return(stored)
@@ -45,7 +46,27 @@ compartmentalize <- function(data, centers=3, inter=FALSE, ...)
 	lost <- which(is.na(mat), arr.ind=TRUE)
 	lost.dist <- abs(mid(ranges(anchors(data[lost[,1]]))) - mid(ranges(targets(data[lost[,2]]))))
 	lost.dist <- log10(lost.dist + exptData(data)$width)
-	mat[is.na(mat)] <- scaledAverage(DGEList(0, mean(data$totals))) - dist2trend(lost.dist)
+	mat[is.na(mat)] <- .makeEmpty(data) - dist2trend(lost.dist)
+	return(mat)
+}
+
+.safeCluster <- function(mat, centers, ...) { 
+	if (nrow(mat) > centers) { 
+		out <- kmeans(mat, centers=centers, ...)
+		comp <- out$cluster
+	} else {
+		comp <- 1:nrow(mat)
+	}
+	return(comp)
+}
+
+.debiasBins <- function(mat) {
+	# Just subtracting half from both rows and columns. This is equivalent to 
+	# dividing by square root of coverage, which works pretty well in place of
+	# a more rigorous iterative approach (check out Rao's supplementaries).
+	rwm <- log2(rowMeans(2^mat))
+	mat <- mat - rwm/2
+	mat <- t(t(mat) - rwm/2)
 	return(mat)
 }
 
